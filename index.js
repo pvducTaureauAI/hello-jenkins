f928526ace082af694424cadc7850ea2d97b835d
fish bird <duc.lhp1@gmail.com>
2025-12-15 21:31:47 +0700

    init hello jenkins

---
+++ index.js
@@ -0,0 +1,17 @@
+const http = require("http");
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hello Jenkins");
+  } else {
+    res.writeHead(404);
+    res.end("Not Found");
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
