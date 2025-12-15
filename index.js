00333a231b8fd7e4e68d3edae6770be71f4cd44e
fish bird <duc.lhp1@gmail.com>
2025-12-15 23:02:37 +0700

    update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = 3000;
 const server = http.createServer((req, res) => {
   if (req.url === "/") {
     res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Hello Jenkins");
+    res.end("Hello Fish");
   } else {
     res.writeHead(404);
     res.end("Not Found");
